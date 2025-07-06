482dd6fa676242ed3fb5b663e050212e531b90f2
Kevin <kvnnedd@gmail.com>
2025-07-06 18:09:40 -0400

    removed console.log statements from todocounter

--- components/TodoCounter.js
+++ components/TodoCounter.js
@@ -3,34 +3,28 @@ export default class TodoCounter {
     this._element = document.querySelector(".counter__text");
     this._completed = todos.filter((todo) => todo.completed).length;
     this._total = todos.length;
-    this._updateText(); 
-    console.log("this._completed", this._completed);
-    console.log("this._total", this._total);
-  };
-  
-    updateCompleted = (increment) => {
-      if (increment) {
-        this._completed += 1;
-      } else {
-        this._completed -= 1;
-      }
-      this._updateText();
-  };
+    this._updateText();
+  }
 
- 
-  updateTotal = (increment) => {
+  updateCompleted = (increment) => {
     if (increment) {
-      this._total +=1;
+      this._completed += 1;
     } else {
-      this._total -=1;
+      this._completed -= 1;
     }
     this._updateText();
- 
   };
 
+  updateTotal = (increment) => {
+    if (increment) {
+      this._total += 1;
+    } else {
+      this._total -= 1;
+    }
+    this._updateText();
+  };
 
   _updateText() {
-
     this._element.textContent = `Showing ${this._completed} out of ${this._total} completed`;
   }
 }
